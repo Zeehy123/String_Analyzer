705457a0cbee71f4bf1148026f638a6617f2c551
Zeehy123 <Zeehy123@users.noreply.github.com>
2025-10-21 11:27:02 +0100

    string/

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import CreateStringAnalysisResultView, ListStringView, GetStringAnalysisResultView,nl_query_view
 
 urlpatterns=[
-  path('strings',CreateStringAnalysisResultView.as_view(),name='create_string'),
-  path("strings/",ListStringView.as_view(),name="list_strings"),
+  path('strings/',CreateStringAnalysisResultView.as_view(),name='create_string'),
+  path("strings",ListStringView.as_view(),name="list_strings"),
   path("strings/<str:string_value>/",GetStringAnalysisResultView.as_view(),name="get_string"),
   path("strings/filter-by-natural-language",nl_query_view,name="nl_query"),
 
